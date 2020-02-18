798d84fc3af2ae54b90c454b5a43c258d51b9736
Murutu <murutupeter@email.com>
2020-02-18 12:14:04 +0300

    Created import ValidationError in the forms.py

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField
 from wtforms.validators import Required,Email,EqualTo
 from ..model import User
+from wtforms import ValidationError
 
 
 class RegistrationForm(FlaskForm):
