b6ecafcebb8155523a5fb860500ce74ee3b06403
Murutu <murutupeter@email.com>
2020-02-18 15:19:49 +0300

    Added class Config in config.py

--- config.py
+++ config.py
@@ -1,4 +1,11 @@
+import os
 class Config:
+    '''
+    General configuration parent class
+    '''
+    debug = True
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://peter:ozil@localhost/saka'
     
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     
