7eced6eea8e8679cdd5e2e0faec2b4935c588c5c
Murutu <murutupeter@email.com>
2020-02-18 12:43:57 +0300

    Created def profile in the views.py

--- app/main/views.py
+++ app/main/views.py
@@ -1,1 +1,11 @@
 from flask_login import login_required
+from flask import render_template,request,redirect,url_for,abort
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
