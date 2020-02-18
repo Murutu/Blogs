b307072f00195aaccb00a491d5693b64197acb01
Murutu <murutupeter@email.com>
2020-02-18 12:03:56 +0300

    Created login_manager_init_app in the __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,9 @@ login_manager.login_view = 'auth.login'
 def create_app(config_name):
     app = Flask(__name__)
     
+#Initializing Flask Extensions
+login_manager.init_app(app)    
+    
     
 # Registering the blueprint
 from .main import main as main_blueprint
