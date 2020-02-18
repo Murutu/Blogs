f061a299d3c38d0884998bce83c4f46e4c417522
Murutu <murutupeter@email.com>
2020-02-18 15:22:06 +0300

    Added class Prod Config in config.py

--- config.py
+++ config.py
@@ -15,4 +15,11 @@ class Config:
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")    
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    
+    
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    
+class     
+            
