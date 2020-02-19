32f7d0064000226f18bdab55cfcf83c75ee84078
Murutu <murutupeter@email.com>
2020-02-19 09:17:56 +0300

    Made changes  in the  __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_uploads import UploadSet,configure_uploads,IMAGES
+from config import config_options
 from flask_login import LoginManager
 from flask_mail import Mail
 
@@ -8,21 +9,27 @@ login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 
+mail = Mail()
 
 photos  = Uploadset('photos',IMAGES)
 def create_app(config_name):
     app = Flask(__name__)
     
-#Initializing Flask Extensions
-db.init_app(app)
-login_manager.init_app(app)
-mail.init_app(app)
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+    
+    #Initializing Flask Extensions
+    db.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
 
-# configure UploadSet
-configure_uploads(app,photos)  
+    # configure UploadSet
+    configure_uploads(app,photos)  
     
     
-# Registering the blueprint
-from .main import main as main_blueprint
-app.register_blueprint(main_blueprint)    
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app    
     
