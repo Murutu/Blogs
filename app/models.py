72c643d1c3d326c7a8e3e2cce959f3f0593e5c0e
Murutu <murutupeter@email.com>
2020-02-19 09:38:29 +0300

    Added password.setter  in the  models.py

--- app/models.py
+++ app/models.py
@@ -17,6 +17,17 @@ class User(db.Model):
 
      def __repr__(self):
          return f'User {self.username}'
+     
+@property
+def password(self):
+    raise AttributeError('You cannot read the password attribute')
+
+@password.setter
+def password(self, password):
+    self.pass_secure = generate_password_hash(password)
+    
+def verify_password(self,password):
+    return check_password_hash(self.pass_secure,password)         
     
 @login_manager.user_loader
 def load_user(user_id):
